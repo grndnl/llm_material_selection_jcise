a398c60bcf84eb1ef82e625b3099cb6e912da5dd
Daniele Grandi <daniele.grandi@autodesk.com>
2024-03-14 15:25:09 -0700

    initial generation upload

---
+++ generation/generation.py
@@ -0,0 +1,99 @@
+from llama_index.llms.replicate import Replicate
+from llama_index.llms.openai import OpenAI
+import os
+import pandas as pd
+from tqdm import tqdm
+
+
+def run_thread(model, question):
+    if model == 'mixtral':
+        # API token of the model/pipeline that we will be using
+        os.environ["REPLICATE_API_TOKEN"] = ""
+        llm = Replicate(model="mistralai/mixtral-8x7b-instruct-v0.1", max_new_tokens=2)
+    elif model == 'gpt-4-0125-preview':
+        # OpenAI model
+        llm = OpenAI(model="gpt-4-0125-preview", max_new_tokens=1)
+    else:
+        raise ValueError("Invalid model")
+
+    response = llm.complete(question)
+    return response.text
+
+
+def compile_question(design, criterion, material, question_type):
+    if question_type == 'zero_shot':
+        question = f"You are a material science and design engineer expert.\n" \
+                   f"You are tasked with designing a {design}. The design should be {criterion}.\n" \
+                   f"How well do you think {material} would perform in this application? Answer on a scale of 1-10, " \
+                   f"where 0 is 'unsatisfactory', 5 is 'acceptable', and 10 is 'excellent', with just the number and no other words."
+    else:
+        raise ValueError("Invalid question type")
+    return question
+
+
+def append_results(results, design, criterion, material, response):
+    # validate response is only a single integer
+    try:
+        response = int(response)
+    except ValueError:
+        try:
+            response = int(response.split()[0])
+            response = int(response)
+        except ValueError:
+            raise ValueError(f"Response is not a single integer: {response}")
+
+    return results._append({
+        'design': design,
+        'criteria': criterion,
+        'material': material,
+        'response': response
+    }, ignore_index=True)
+
+
+if __name__ == '__main__':
+    materials = [
+        "Steel",
+        "Aluminium",
+        # "Titanium",
+        # "Glass",
+        # "Wood",
+        # "Thermoplastic",
+        # "Elastomer",
+        # "Thermoset",
+        # "Composite"
+    ]
+
+    designs = [
+        "Kitchen Utensil Grip",
+        "Spacecraft Component",
+        # "Underwater Component",
+        # "Safety Helmet"
+    ]
+
+    criteria = [
+        "Lightweight",
+        "Heat resistant",
+        # "Corrosion resistant",
+        # "High strength"
+    ]
+
+    for question_type in ['zero_shot']:  # , 'context', 'parallel', 'chain-of-thought', "fine-tuned", 'temperature']:
+        for model in ['gpt-4-0125-preview']:  # , 'mixtral']:
+            #initialize results dataframe
+            results = pd.DataFrame(columns=['design', 'criteria', 'material', 'response'])
+
+            if question_type == 'parallel':
+                materials = [", ".join(materials)]
+
+            for design in tqdm(designs, desc=f"Running {question_type} questions for {model}"):
+                for criterion in criteria:
+                    for material in materials:
+                        if question_type != 'chain-of-thought':
+                            question = compile_question(design, criterion, material, question_type)
+                            response = run_thread(model, question)
+                        else:
+                            raise ValueError("Invalid question type")
+
+                        results = append_results(results, design, criterion, material, response)
+
+            results.to_csv(f"answers/{question_type}_{model}.csv", index=False)
