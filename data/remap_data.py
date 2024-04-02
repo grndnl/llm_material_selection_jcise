b417c8125517b1ce62e1a7ebcb09e635500f1236
Daniele Grandi <daniele.grandi@autodesk.com>
2024-04-02 14:05:57 -0700

    dropping people who did not have material selection experience, and downsampling question 1

--- data/remap_data.py
+++ data/remap_data.py
@@ -4,6 +4,10 @@ import pandas as pd
 file_path = 'survey_responses.csv'
 df = pd.read_csv(file_path)
 
+# drop people that did not have material experience
+df = df[df['material_familiarity'] == 'Yes']
+
+
 # Mapping questions to 'design' and 'criteria'
 design_map = {
     range(1, 5): "kitchen utensil grip",
@@ -60,7 +64,7 @@ mapped_data = []
 
 # Extract and map the data from the original DataFrame
 for q_num in range(1, 17):
-    q_cols = [f"Q{q_num}_{material}" for material in materials]
+    # q_cols = [f"Q{q_num}_{material}" for material in materials]
     for material in materials:
         for index, row in df.iterrows():
             mapped_data.append({
@@ -73,7 +77,17 @@ for q_num in range(1, 17):
 # Convert the mapped data to a DataFrame
 mapped_df = pd.DataFrame(mapped_data)
 
+# downsample the kitchen utensil grip and lightweight to 750
+filtered_df = mapped_df[(mapped_df['design'] == 'kitchen utensil grip') & (mapped_df['criteria'] == 'lightweight')]
+downsampled_df = filtered_df.sample(n=800, random_state=2)
+mapped_df = pd.concat([mapped_df[~((mapped_df['design'] == 'kitchen utensil grip') & (mapped_df['criteria'] == 'lightweight'))], downsampled_df])
+
+
 # Show a sample of the new DataFrame to verify the transformation
 print(mapped_df.head())
+
+print(mapped_df.groupby(['design', 'criteria'])['response'].count().reset_index(name='count'))
+
+print(mapped_df.count())
 
 mapped_df.to_csv('survey_responses_mapped.csv', index=False)
