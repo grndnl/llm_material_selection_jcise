3e5250962516934f827cd01111b2344b20989e0d
Daniele Grandi <daniele.grandi@autodesk.com>
2024-04-09 14:48:28 -0700

    app update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,13 @@
 import streamlit as st
+from streamlit_feedback import streamlit_feedback
+
 
 st.title("Get material feedback from GPT-4")
 
 st.header("What would you like to design?")
+
+feedback = streamlit_feedback(
+    feedback_type="thumbs",
+    # optional_text_label="[Optional] Please provide an explanation",
+)
+feedback
